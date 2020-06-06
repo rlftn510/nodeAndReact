01dabc732c6fd54cd986371de9762e99e70f3de5
류길수 <>
2020-06-06 18:23:42 +0900

    mongo DB 연결 및 Schma  와 Model

--- bolier-plate/index.js
+++ bolier-plate/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://rlftn510:qwe123@cluster0-shard-00-00-pybch.mongodb.net:27017,cluster0-shard-00-01-pybch.mongodb.net:27017,cluster0-shard-00-02-pybch.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true,
+   useFindAndModify: false
+}).then(() => console.log('good!'))
+   .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!!!!!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
