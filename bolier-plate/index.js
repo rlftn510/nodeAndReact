d5116f3256a70791b4cc58a35ed26f16f4288d03
류길수 <>
2020-06-07 19:14:56 +0900

    비밀정보 보호

--- bolier-plate/index.js
+++ bolier-plate/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://rlftn510:qwe123@cluster0-shard-00-00-pybch.mongodb.net:27017,cluster0-shard-00-01-pybch.mongodb.net:27017,cluster0-shard-00-02-pybch.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
    useCreateIndex: true,
@@ -11,7 +20,23 @@ mongoose.connect('mongodb://rlftn510:qwe123@cluster0-shard-00-00-pybch.mongodb.n
 }).then(() => console.log('good!'))
    .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!!!!!'))
+app.get('/', (req, res) => res.send('Hello World!!!!! GIlsu'))
+
+app.post('/register', (req, res) => {
+   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+   // 그것들을 데이터 베이스에 넣어준다
+
+   const user = new User(req.body);
+
+   user.save((err, userInfo) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+         success: true
+      })
+   })
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
