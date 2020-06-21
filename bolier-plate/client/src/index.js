ae90842fba8194ca5cf8cb3c9b5f492763ac28bc
류길수 <>
2020-06-21 19:43:21 +0900

    react redux 세팅

--- bolier-plate/client/src/index.js
+++ bolier-plate/client/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux'
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk'
+import Reducer from './_reducer'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && 
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+  >
+      <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
