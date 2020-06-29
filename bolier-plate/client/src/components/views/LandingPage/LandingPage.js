50f525b003cf75e04609c5be961c707d0c173e3b
류길수 <>
2020-06-29 23:44:39 +0900

    redux 이용 로그인 기능

--- bolier-plate/client/src/components/views/LandingPage/LandingPage.js
+++ bolier-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -8,8 +8,8 @@ function LandingPage() {
         }, [])
     })
     return (
-        <div>
-            LandingPageasdasd
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+            시작페이지
         </div>
     )
 }
