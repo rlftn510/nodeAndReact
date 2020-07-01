3389c01647dbb0e035ac7d3428dcda8cf6287608
류길수 <>
2020-07-02 00:00:31 +0900

    auth hoc

---
+++ bolier-plate/client/src/hoc/auth.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react'
+import Axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { auth } from '../_actions/user_actions'
+
+export default function (SpecificComponent, option, adminRoute = null) {
+
+    //option
+    //null      => 아무나 출입가능
+    //true      => 로그인한 유저 출입가능
+    //false     => 로그인한 유저 출입 불가능    
+
+
+    function AuthenticationCheck(props) {
+        const dispatch = useDispatch()
+        
+        useEffect(() => {
+            dispatch(auth()).then(response =>{
+                console.log(response)
+                //로그인 하지 않은 상태
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history.push('/login')
+                    }
+                } else {
+                    //로그인 상태
+                    if(adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+                    } else {
+                        if (option === false) 
+                            props.history.push('/')
+                    }
+                }
+            })
+            
+        }, [])
+        
+        return (
+            <SpecificComponent/>
+        )
+    }
+    
+    return AuthenticationCheck
+}
